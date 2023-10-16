3408fa2a39f9f6f52a6001a499b3ad0b2e9b8196
Luke McPhee <lukemcphee1@icloud.com>
2023-10-16 22:23:46 +0100

    working but can't call random id

---
+++ lib/repo/util.ml
@@ -0,0 +1,18 @@
+let get_uri () = "postgres://example-username:pass@localhost:5432/shorty-db"
+
+let connect () =
+  let uri = get_uri () in
+  Caqti_lwt_unix.connect (Uri.of_string uri)
+
+(** For `utop` interactions interactions. See `README.md`.
+ *)
+let connect_exn () =
+  let conn_promise = connect () in
+  match Lwt_main.run conn_promise with
+  | Error err ->
+      let msg =
+        Printf.sprintf "Abort! We could not get a connection. (err=%s)\n"
+          (Caqti_error.show err)
+      in
+      failwith msg
+  | Ok module_ -> module_
