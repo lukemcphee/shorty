10157c120915c60ec4acf3db10a922a7c3183da4
Luke McPhee <lukemcphee1@icloud.com>
2023-10-17 09:12:11 +0100

    tests passing

--- test/repo/db.ml
+++ test/repo/db.ml
@@ -2,6 +2,11 @@ let str_error promise =
   Lwt.bind promise (fun res ->
       res |> Result.map_error Caqti_error.show |> Lwt.return)
 
+
+let drop_first_from_triple triple = let (_, b, c) = triple in
+                                    (b,c)
+
+
 let%test_unit "PostgreSQL: add (asynchronously)" =
   let ( => ) = [%test_eq: (Base.int, Base.string) Base.Result.t] in
   let will_add a b =
@@ -14,7 +19,7 @@ let%test_unit "PostgreSQL: add (asynchronously)" =
 let%test_unit "Able to add to the database" =
   let ( => ) =
     [%test_eq:
-      ( (Base.int * Base.string * Base.string) Base.list,
+      ( (Base.string * Base.string) Base.list,
         Base.string )
       Base.Result.t]
   in
@@ -24,6 +29,6 @@ let%test_unit "Able to add to the database" =
     let ( let* ) = Lwt_result.bind in
     let* conn = Repo.Util.connect () |> str_error in
     let* _ = Repo.Db.insert conn input_url target_url |> str_error in
-    Repo.Db.find_all conn |> str_error
+    Lwt_result.bind (Repo.Db.find_all conn ) (fun res -> Lwt_result.return @@ List.map drop_first_from_triple res)|> str_error
   in
-  Lwt_main.run add_entry => Ok [ (1, input_url, target_url) ]
+  Lwt_main.run add_entry => Ok [ (input_url, target_url) ]
